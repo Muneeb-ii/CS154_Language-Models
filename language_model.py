f0ef0f72a403690d6e8e29a7fdcb546904981874
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2024-10-09 14:04:05 -0400

    Added Lemmatization

--- language_model.py
+++ language_model.py
@@ -1,18 +1,27 @@
 from helper import get_file_contents
+from nltk.stem import WordNetLemmatizer
+
+wnl = WordNetLemmatizer()
 
 list_of_stop_words: str = get_file_contents("list_of_stop_words.txt")
 characters_to_remove = ["!", ",", ".", "?", ":", ";", "-", "_"]
 translation_table = str.maketrans("", "", "".join(characters_to_remove))
 
 
-def preprocess_text(text: str) -> str:
+def preprocess_text(text: str) -> set[str]:
     text_lower: str = text.lower()
     text_no_punctuation: str = text_lower.translate(translation_table)
     text_tokenized: list[str] = text_no_punctuation.split(" ")
     text_no_stop_words: list[str] = [
         f for f in text_tokenized if f not in list_of_stop_words
     ]
-    return text_no_stop_words
+    text_stemmed: list[str] = [wnl.lemmatize(f, pos="v") for f in text_no_stop_words]
+    return set(text_stemmed)
+
+
+movie_review: str = "This was a fantastic movie. I loved it so much. It was a great time watching this movie"
+preprocessed_text: list[str] = preprocess_text(movie_review)
+print(preprocessed_text)
 
 
 def get_unique_words(text: list[str]) -> set[str]:
